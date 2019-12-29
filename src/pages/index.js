ad7eed327e51824dcc2cd8d26d82825fcd9cac9d
Mohammed Yahya <my.almusaddar@gmail.com>
2019-12-29 19:56:43 +0200

    initia commit

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+
+class IndexPage extends React.Component {
+  render() {
+    const siteTitle = "mhmd.io 🚀"
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="Home"
+          keywords={[`blog`, `mohammed`, `yahya`, `almusaddar`, `website`]}
+        />
+        {/* <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" /> */}
+        <h1>
+          Salam people{" "}
+          <span role="img" aria-label="wave emoji">
+            👋
+          </span>
+        </h1>
+        <p>I’m Mohammed Yahya, from Gaza, Palestine.</p>
+        <p>
+          I’m working on Developing Cloud Solutions and Automation Projects
+          using Terraform, Ansible and other Open Source tools.
+        </p>
+        <Link to="/blog/">
+          <Button marginTop="10px">Go to Blog</Button>
+        </Link>
+        <Link to="/images/">
+          <Button marginTop="10px">Go to Images</Button>
+        </Link>
+        <Link to="/services/">
+          <Button marginTop="10px">Go to Services</Button>
+        </Link>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
