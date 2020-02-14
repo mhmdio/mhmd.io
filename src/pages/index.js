7e81d5ccc120351c53048dfd4d1427a4224cbaed
Mohammed Yahya <my.almusaddar@gmail.com>
2020-02-14 20:09:03 +0200

    add new links

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,9 +37,33 @@ class IndexPage extends React.Component {
           I’m working on Developing Cloud Solutions and Automation Projects
           using Pulumi, Terraform, Vault, Ansible and other Open Source tools.
         </p>
-        <Link to="/blog/">
-          <Button marginTop="10px">Go to Blog</Button>
-        </Link>
+        <p>
+          {" "}
+          <Link to="/blog/">
+            <Button marginTop="10px">Go to Blog</Button>
+          </Link>
+        </p>
+
+        <p>
+          <a href="https://twitter.com/mhmd_io">
+            <Button marginTop="10px">Follow me on Twitter</Button>
+          </a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/mhmdio">
+            <Button marginTop="10px">Connect on LinkedIn</Button>
+          </a>
+        </p>
+        <p>
+          <a href="https://keybase.io/mhmd">
+            <Button marginTop="10px">Verify me on keybase</Button>
+          </a>
+        </p>
+        <p>
+          <a href="https://learn.acloud.guru/profile/mohammed-yahya">
+            <Button marginTop="10px">ACG Academic Transcript</Button>
+          </a>
+        </p>
         {/* <Link to="/images/">
           <Button marginTop="10px">Go to Images</Button>
         </Link>
